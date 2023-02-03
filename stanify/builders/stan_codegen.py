5ba914009d161f12bd8d71af6f9d27155109352f
Dashadower <tttllshin@gmail.com>
2023-02-04 00:25:07 +0900

    Add subscript ordering logic for model block codegen

--- stanify/builders/stan_codegen.py
+++ stanify/builders/stan_codegen.py
@@ -11,6 +11,7 @@ from pathlib import Path
 import numpy as np
 from .vensim_ast_walker import BaseVensimWalker
 import pysd.translators.structures.abstract_expressions as pysd_ast
+from collections import defaultdict
 from numbers import Number
 
 
@@ -90,11 +91,144 @@ class TransformedDataCodegenVensimWalker(BaseVensimWalker, StanBlockCodegen):
                 stan_dtype = "int" if np.issubdtype(component_ast.dtype, np.integer) else "real"
 
                 # Stan requires that array values be initialized with braces, instead of brackets.
-                # We take the numpy array values, convert them to a python list, and then replace all braces in the
-                # stringified list with brackets.
-                stan_array_init = str(component_ast.tolist()).replace("{", "[").replace("}", "]")
+                # We take the numpy array values, convert them to a python list, and then replace all brackets in the
+                # stringified list with braces.
+                stan_array_init = str(component_ast.tolist()).replace("[", "{").replace("]", "}")
 
                 self._code += f"array[{','.join(str(dim) for dim in component_ast.shape)}] {stan_dtype} {node_name} = {stan_array_init};\n"
+
+
+class ModelBlockCodegen(StanBlockCodegen):
+    """
+    Generates code for the `model` Stan block. This includes prior and likelihood specifications.
+
+    Used for:
+    - data2draws
+    """
+
+    def generate(self, v2s_code_handler: Vensim2StanCodeHandler, vensim_model_context: VensimModelContext) -> None:
+        # Find subscripted variables, and figure out the way to loop through them with the minimum amount of loops.
+        # If we have three variables, say with subscript usage (a, b), (b, c), (c), (a), it would be best to run like
+        # the following loop:
+        # for a in A:
+        #   (a)
+        #   for b in B:
+        #       (a, b)
+        #
+        # for c in C:
+        #   (c)
+        #   for b in B:
+        #       (b, c)
+        #
+        # cost = 2 * 2 + 2 * 2
+
+        # Problem definition - minimize the total cost of loops while ensuring all subscript combinations are used.
+        # This felt really similar to Quine-McClusky; I had to find the "combinations" that covered all subscripts with
+        # minimizing rendundant subscript.
+
+        # This set holds all the subscript combinations that need to be covered
+        used_subscripts = set()
+
+        # Iterate through all declared LHS-variables and record what subscript was used with what subscript.
+        for _, declared_variable in v2s_code_handler.declared_variables.items():
+            if declared_variable.subscripts:
+                used_subscripts.add(set(declared_variable.subscripts))
+
+        # We will use a simple tree structure to hold the hierarchical order of nested for loops.
+        @dataclass
+        class Node:
+            value: str
+            children: set["Node"] = field(default_factory=set)
+
+            def __hash__(self):
+                return hash(self.value)
+
+        # We're going to recursively iterate through the subscript sets. This helps when we have triple-subscripted
+        # variables, and maybe even more!
+        # I'll try my best to write out my thought process
+
+        # This is the driving function
+        def find_order(subscript_combinations: list[set[str]], parent: Node):
+            """
+
+            Parameters
+            ----------
+            subscript_combinations : list[set[str]]
+                The current subscripts that we need to find out the ordering for
+            parent : Node
+                parent Node
+            """
+
+            # For each single subscript value, record what subscript combinations contain the value
+            usages: dict[str, set[frozenset[str]]] = defaultdict(set)
+
+            for combination in subscript_combinations:
+                for subscript in combination:
+                    usages[subscript].add(frozenset(combination))
+
+            # We sort the subscript values base on the most used subscript values.
+            sorted_subscripts = sorted(usages.items(), key=lambda x: len(x[1]), reverse=True)
+
+            # This set holds the subscript combinations that are already covered
+            covered = set()
+
+            # Iterate through The usage dictionary, checking whether we need to add the subscript value
+            for subscript, combinations in sorted_subscripts:
+                required = False
+                current_node = Node(subscript)
+
+                # This list holds the subscript combination with the current subscript value removed.
+                # For example, if we had (a, b, c) and (a, c) and we're currently at a, it would contain
+                # (b, c) and (c,)
+                self_removed = []
+
+                for combination in combinations:
+                    # If there is a subscript combination that's not yet covered, we need the current subscript value
+                    if combination not in covered:
+                        required = True
+                    else:
+                        continue
+                    covered.add(combination)
+
+                    # Create the subscript combination with current value removed
+                    subtracted = frozenset(combination - {subscript})
+                    if subtracted:
+                        self_removed.append(subtracted)
+
+                # We run the functoin again, but this time with the child subscripts(that doesnn't have the current
+                # value anymore), and parent becomes the current subscript value.
+                find_order(self_removed, current_node)
+
+                # We only add it to the parent if it's required(the subscript has a new combination)
+                if required:
+                    parent.children.add(current_node)
+
+
+            # Create a dummy node to hold the results
+            root = Node("")
+            find_order(list(used_subscripts), root)
+
+            # Now we run code generation by traversing through the V2S AST
+            # Keep a set of V2S variables we've already processed
+            completed_v2s_variables = set()
+
+            def build_forloops(node: Node, current_subscript: set[str], nest_level=0):
+                current_subscript.add(node.value)
+                if not node:
+                    return
+
+                loop_bound_variable = chr(ord("i") + nest_level)  # i, j, k, l, ...
+                self._code += f"for ({loop_bound_variable} in 1:{node.value}) {{\n"
+                self._code.indent_level += 1
+
+                for children in node.children:
+                    build_forloops(children, current_subscript, nest_level + 1)
+                self._code.indent_level -= 1
+                self._code += "}}\n"
+
+            for child in root.children:
+                build_forloops(child, set())
+
 
 
 
